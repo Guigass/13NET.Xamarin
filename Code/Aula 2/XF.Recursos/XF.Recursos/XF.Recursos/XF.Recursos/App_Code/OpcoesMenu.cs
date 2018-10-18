9f78c7b9cfdf9cf4ad9a1489d74a0e3a6b9d0915
Guilherme Gentile <uk.gentile@gmail.com>
2018-10-17 22:21:25 -0300

    Explicando Atividade

---
+++ Code/Aula 2/XF.Recursos/XF.Recursos/XF.Recursos/XF.Recursos/App_Code/OpcoesMenu.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XF.Recursos.App_Code
+{
+    public class OpcoesMenu
+    {
+        public string Descricao { get; set; }
+        public ImageSource Icone { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
---
+++ Code/Aula 2/XF.Recursos/XF.Recursos/XF.Recursos/XF.Recursos/Menu/MenuView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.Recursos.App_Code;
+using XF.Recursos.Controles;
+
+namespace XF.Recursos.Menu
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class MenuView : ContentPage
+	{
+        public ListView ListView { get { return lstMenu; } }
+        public MenuView()
+        {
+            InitializeComponent();
+
+            ObservableCollection<OpcoesMenu> menuItems = new ObservableCollection<OpcoesMenu>();
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Home",
+                Icone = "Home.png",
+                TargetType = typeof(HomeView)
+            });
+
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Editor",
+                Icone = "Home.png",
+                TargetType = typeof(EditorView)
+            });
+
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Picker",
+                Icone = "Home.png",
+                TargetType = typeof(PickerView)
+            });
+
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "List Picker View",
+                Icone = "Home.png",
+                TargetType = typeof(ListPickerView)
+            });
+
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Stepper View",
+                Icone = "Home.png",
+                TargetType = typeof(StepperView)
+            });
+
+            menuItems.Add(new OpcoesMenu
+            {
+                Descricao = "Progresso View",
+                Icone = "Home.png",
+                TargetType = typeof(ProgressoView)
+            });
+
+            lstMenu.ItemsSource = menuItems;
+        }
+    }
+}
