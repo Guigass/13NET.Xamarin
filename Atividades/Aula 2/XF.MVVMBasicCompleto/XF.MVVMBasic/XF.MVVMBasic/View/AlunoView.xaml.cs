be296cebe1424c434ec93c6a4f982445609b2031
Guilherme Gentile <uk.gentile@gmail.com>
2018-10-24 20:50:52 -0300

    Inicio Aula 3

---
+++ Atividades/Aula 2/XF.MVVMBasicCompleto/XF.MVVMBasic/XF.MVVMBasic/View/AlunoView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XF.MVVMBasic.ViewModel;
+
+namespace XF.MVVMBasic.View
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class AlunoView : ContentPage
+	{
+        AlunoViewModel vmAluno;
+
+        public AlunoView()
+        {
+            var aluno = AlunoViewModel.GetAluno();
+            vmAluno = new AlunoViewModel(aluno);
+            BindingContext = vmAluno;
+
+            InitializeComponent();
+        }
+    }
+}
---
+++ Atividades/Aula 2/XF.MVVMBasicCompleto/XF.MVVMBasic/XF.MVVMBasic/ViewModel/AlunoViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XF.MVVMBasic.Model;
+
+namespace XF.MVVMBasic.ViewModel
+{
+    public class AlunoViewModel
+    {
+        #region Propriedades
+        public string RM { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        #endregion
+
+        public AlunoViewModel(Aluno aluno)
+        {
+            this.RM = aluno.RM;
+            this.Nome = aluno.Nome;
+            this.Email = aluno.Email;
+        }
+
+        public static Aluno GetAluno()
+        {
+            var aluno = new Aluno()
+            {
+                Id = Guid.NewGuid(),
+                RM = "542621",
+                Nome = "Anderson Silva",
+                Email = "anderson@ufc.com"
+            };
+            return aluno;
+        }
+    }
+
+}
