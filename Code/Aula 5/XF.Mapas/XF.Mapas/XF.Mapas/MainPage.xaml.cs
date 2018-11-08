1ba71fe1e2374dae2e7b6ef1eec7caf84851eb5f
Guilherme <uk.gentile@gmail.com>
2018-11-07 22:58:26 -0200

    fim da aula, adicionado novos controles do google maps

--- Code/Aula 5/XF.Mapas/XF.Mapas/XF.Mapas/MainPage.xaml.cs
+++ Code/Aula 5/XF.Mapas/XF.Mapas/XF.Mapas/MainPage.xaml.cs
@@ -18,5 +18,20 @@ namespace XF.Mapas
         {
             Navigation.PushAsync(new View.MapaView());
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.ControleView());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.LocalizacaoView());
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new View.MarcarView());
+        }
     }
 }
